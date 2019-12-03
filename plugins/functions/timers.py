7a1451adb3c1e88f1dff90afbb012ddcfabb880b
Xiao <xiao@scp-079.org>
2019-12-03 10:06:01 +0000

    - 0.1.2-debug

--- plugins/functions/timers.py
+++ plugins/functions/timers.py
@@ -59,6 +59,10 @@ def backup_files(client: Client) -> bool:
 
 def interval_hour_01() -> bool:
     # Execute every hour
+
+    # TODO Debug
+    logger.warning("Acquiring timer lock...")
+
     glovar.locks["text"].acquire()
     glovar.locks["message"].acquire()
     try:
@@ -83,6 +87,9 @@ def interval_hour_01() -> bool:
     except Exception as e:
         logger.warning(f"Interval hour 01 error: {e}", exc_info=True)
     finally:
+        # TODO Debug
+        logger.warning("Releasing timer lock...")
+
         glovar.locks["text"].release()
         glovar.locks["message"].release()
 
